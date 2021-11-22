5164c0cc246663da373ff93cc94c9fb07067ebca
Tanzeem Rahat <75111696+TangoRomeo80@users.noreply.github.com>
2021-11-22 18:54:44 +0600

    Updated main.py

--- contemporary_artist_beggerizer/main.py
+++ contemporary_artist_beggerizer/main.py
@@ -1,1 +1,44 @@
-print('hi')
+import colorgram
+import turtle as t
+import random as rand
+
+color_list = []
+colors = colorgram.extract('hirst_img.jpg', 30)
+for color in colors:
+    if color.rgb.r < 220 and color.rgb.g < 220 and color.rgb.b < 220:
+        r = color.rgb.r
+        g = color.rgb.g
+        b = color.rgb.b
+        new_color = (r, g, b)
+        color_list.append(new_color)
+
+t.colormode(255)
+tim = t.Turtle()
+screen = t.Screen()
+
+
+def more_dots(x, y):
+
+    tim.goto(x, y)
+    for i in range(10):
+        screen.colormode(255)
+        tim.dot(20, rand.choice(color_list))
+        tim.forward(50)
+
+
+def main():
+    tim.hideturtle()
+    tim.penup()
+    tim.speed("fastest")
+    x_pos = -225
+    y_pos = -225
+
+    for i in range(10):
+        more_dots(x_pos, y_pos)
+        y_pos += 50
+
+    screen.exitonclick()
+
+
+main()
+
