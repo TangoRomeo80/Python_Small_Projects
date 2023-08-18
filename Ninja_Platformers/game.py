9a005475a4c82f9d7f2f1eeff40478a7133ff958
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-08-18 10:19:26 +0600

    Completed upto collision etection test

--- Ninja_Platformers/game.py
+++ Ninja_Platformers/game.py
@@ -15,11 +15,34 @@ class Game:
         self.screen = pygame.display.set_mode((640, 480)) # Define the screen size
         # Define the game clock to force the game to run at a certain fps
         self.clock = pygame.time.Clock()
+        # Load a sample image
+        self.img = pygame.image.load("data/images/clouds/cloud_1.png")
+        self.img.set_colorkey((0, 0, 0))
+        # Set Image position for movement
+        self.img_pos = [160, 260]
+        self.movement = [False, False]
+        # Define area for collision deteciton
+        self.collision_area = pygame.Rect(50, 50, 300, 50)
+
 
     # Function to run the game loop
     def run(self):
         # Run an infinite loop to keep the game running
         while True:
+            # Fill the screen with sky blue color
+            self.screen.fill((14, 219, 248))
+            # Get image rectangle
+            img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            if img_r.colliderect(self.collision_area):
+                pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            else:
+                pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+            
+            # Update the image position
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            # Draw the image on the screen
+            self.screen.blit(self.img, self.img_pos)
+
             # Check for events
             for event in pygame.event.get():
                 # Check if the user wants to quit
@@ -27,6 +50,19 @@ class Game:
                     # Quit the game
                     pygame.quit()
                     sys.exit()
+                # Check if the user pressed a key
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = True
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = True
+                # Check if the user released a key
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        self.movement[0] = False
+                    if event.key == pygame.K_DOWN:
+                        self.movement[1] = False
+
 
             pygame.display.update() # Update the screen
             self.clock.tick(60) # Force the game to run at 60 fps
