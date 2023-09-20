f8bc0eb70fa2727d9c2e17691ac00a342a1739a7
Tanzeem Rahat <tanzeemrahat80@gmail.com>
2023-09-20 14:45:05 +0600

    implemented tile rendering optimization

--- Ninja_Platformers/scripts/tilemap.py
+++ Ninja_Platformers/scripts/tilemap.py
@@ -37,7 +37,14 @@ class Tilemap:
         for tile in self.offgrid_tiles:
             surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] - offset[0], tile['pos'][1] - offset[1]))
 
+        for x in range(offset[0] // self.tile_size, (offset[0] + surf.get_width()) // self.tile_size + 1):
+            for y in range(offset[1] // self.tile_size, (offset[1] + surf.get_height()) // self.tile_size + 1):
+                if str(x) + ';' + str(y) in self.tilemap:
+                    # If the tile is in the tilemap, render it
+                    tile = self.tilemap[str(x) + ';' + str(y)]
+                    surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+
         # Render the tiles
-        for loc in self.tilemap:
-            tile = self.tilemap[loc]
-            surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
+        # for loc in self.tilemap:
+        #     tile = self.tilemap[loc]
+        #     surf.blit(self.game.assets[tile['type']][tile['variant']], (tile['pos'][0] * self.tile_size - offset[0], tile['pos'][1] * self.tile_size - offset[1]))
